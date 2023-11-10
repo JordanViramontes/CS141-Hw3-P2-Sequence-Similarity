53e28d6ad5b4b9f21765e396e3dd5a9e90ccdfbd
clefalto <113867060+clefalto@users.noreply.github.com>
2023-11-09 17:58:57 -0800

    fix printstrings
    4/4 autograder!

--- main.cpp
+++ main.cpp
@@ -23,26 +23,12 @@ const double INSERT = -0.2;
 const double SUB = -0.1; // G<->A, C<->A, G<->T, C<->T
 const double ALT_SUB = -0.15; // C<->G, T<->A
 
-
-
 int main(int argc, char *argv[]) {
     string x, y;
-    if (argc <= 1) {
-        x = "";
-        y = "";
-    }
-    else if (argc == 2) {
-        x = argv[1];
-        y = "";
-    }
-    else {
-        x = argv[1];
-        y = argv[2];
-    }
-    x = "GTACTCAG";
-    y = "GGTACTAG";
-    cout << "\nx: " << x << "\ty: " << y << endl;
-    cout << sequence(x, y) << endl;
+
+    cin >> x;
+    cin >> y;
+    sequence(x, y);
     return 0; 
 }
 
@@ -94,16 +80,21 @@ double sequence(const string &x, const string &y) {
     // initialize to -0.01 because that is impossible to get from adding in any combination/order the score values we have.
     // first index is the row, second index is the column
 
-    // vector<vector<double>> scores( x.size()+1, vector<double>(y.size()+1, -0.01) ); 
+    //vector<vector<double>> scores( x.size()+1, vector<double>(y.size()+1, -0.01) ); 
     vector<vector<cell>> scores(x.size()+1, vector<cell>(y.size()+1));
 
     double returnval = sequenceH(x, y, x.size(), y.size(), scores);
-    print2dvec(x, y, scores);
+    //print2dvec(x, y, scores);
     string xFinal = "";
     string yFinal = "";
     printStrings(x, y, xFinal, yFinal, x.size(), y.size(), scores);
 
-    cout << xFinal << endl << yFinal << endl;
+    // printStrings outputs the strings in reverse order, so we have to reverse them 
+    reverse(xFinal.begin(), xFinal.end());
+    reverse(yFinal.begin(), yFinal.end());
+
+    cout << returnval << "\n";
+    cout << xFinal << "\n" << yFinal << "\n";
     return returnval;
 }
 
@@ -146,40 +137,44 @@ void print2dvec(const string &x, const string &y, const vector<vector<cell>> &ve
     }
 }
 
-void printStrings(const string &x, const string &y, string &xFin, string &yFin, int i, int j, const vector<vector<cell>> &vec) {
+// reverse strings after this is called
+void printStrings(const string &x, const string &y, string &xFinal, string &yFinal, int i, int j, const vector<vector<cell>> &vec) {
+    // xFinal and yFinal should be empty strings :)
+    
     //base cases
     if (i == 0 && j == 0) { //at (0,0)
         return;
     }
     else if (i == 0) { //vertical border
-        cout << "i: " << i << "\tj: " << j << "\tvalue:" << vec.at(i).at(j).value << endl;
-        xFin.insert(0, 1, '-');
-        yFin.insert(0, 1, y.at(j));
-        return printStrings(x, y, xFin, yFin, i, j-1, vec);
+        xFinal.push_back('-');
+        yFinal.push_back(y.at(j-1));
+        return printStrings(x, y, xFinal, yFinal, i, j-1, vec);
     }
     else if (j == 0) { //horizontal border
-        xFin.insert(0, 1, x.at(i));
-        yFin.insert(0, 1, '-');
-        return printStrings(x, y, xFin, yFin, i-1, j, vec);
+        xFinal.push_back(x.at(i-1));
+        yFinal.push_back('-');
+        return printStrings(x, y, xFinal, yFinal, i-1, j, vec);
     }
-    
-    if (vec.at(i).at(j).diag) {
-        xFin.insert(0, 1, x.at(i-1));
-        yFin.insert(0, 1, y.at(j-1));
-        return printStrings(x, y, xFin, yFin, i-1, j-1, vec);
+
+    // check conditions in reverse order since we are starting from the back
+    if (vec.at(i).at(j).ver) {
+        xFinal.push_back('-');
+        yFinal.push_back(y.at(j-1));
+        return printStrings(x, y, xFinal, yFinal, i, j-1, vec);
     }
     else if (vec.at(i).at(j).hor) {
-        xFin.insert(0, 1, x.at(i-2));
-        yFin.insert(0, 1, '-');
-        return printStrings(x, y, xFin, yFin, i-1, j, vec);
+        xFinal.push_back(x.at(i-1));
+        yFinal.push_back('-');
+        return printStrings(x, y, xFinal, yFinal, i-1, j, vec);
     }
-    else if (vec.at(i).at(j).ver) {
-        xFin.insert(0, 1, '-');
-        yFin.insert(0, 1, y.at(j-2));
-        return printStrings(x, y, xFin, yFin, i, j-1, vec);
+    else if (vec.at(i).at(j).diag) {
+        xFinal.push_back(x.at(i-1));
+        yFinal.push_back(y.at(j-1));
+        return printStrings(x, y, xFinal, yFinal, i-1, j-1, vec);
     }
     return;
-}
+}       
+
 // original one by me joran times lmfao (super brute force and awful and terrible and clef made many improvements)
 // double sequence(const string x, const string y, unsigned int i, unsigned int j) {
 //     // cout << "xAT: " << x.at(i) << "\tyAT: " << y.at(j) << endl;
